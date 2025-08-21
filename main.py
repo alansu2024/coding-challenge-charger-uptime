4b6f962be6c1bc8bc2aa51b57d0b26e525239cd8
alansu2024 <156165521+alansu2024@users.noreply.github.com>
2025-08-20 17:40:15 -0700

    read file into stations and reports

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python3
 
+import argparse
+
+from data import Station, UptimeReport
+
 def main() -> int:
-    print("in main")
+    parser = argparse.ArgumentParser(description="Charger Uptime Report")
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    print(f"in main {args.filename=}")
+
+    with open(args.filename, "r") as file:
+        lines = [l.strip() for l in file.readlines()]
+        # for idx, line in enumerate(lines):
+        #     print(f"{idx}: {line()}")
+
+        # drop any leading empty lines
+        while lines[0] == "":
+            lines = lines[1:]
+        
+        if lines[0] != "[Stations]":
+            raise ValueError("expected [Stations] at the start of the file")
+        
+        lines = lines[1:]  # skip the header line
+        while lines[0] != "[Charger Availability Reports]":
+            line, lines = lines[0], lines[1:]
+            if line == "":
+                continue
+            station = Station.from_station_line(line)
+            print(f"Station {station.id} with chargers {station.charger_ids}")
+
+        lines = lines[1:]  # skip the header line
+        for line in lines:
+            if line == "":
+                continue
+            report = UptimeReport.from_report_line(line)
+            print(f"Report {report.id} from {report.start_time_nanos} to {report.end_time_nanos}, up: {report.up}")
+
     return 0
 
 if __name__ == "__main__":
