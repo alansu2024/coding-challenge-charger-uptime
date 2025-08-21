6a3d23c8116eab96674b40a10efd4e4620784c7c
alansu2024 <156165521+alansu2024@users.noreply.github.com>
2025-08-20 17:10:16 -0700

    basic data model and rudimentary tests

---
+++ data.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+@dataclass
+class UptimeReport:
+    id: str
+    start_time_nanos: int
+    end_time_nanos: int
+    up: bool
+
+    @staticmethod
+    def from_report_line(line: str) -> UptimeReport:
+        elts: list[str] = line.split(" ")
+        if len(elts) != 4:
+            raise ValueError(f"failed to parse report line: {line}")
+
+        match elts[3]:
+            case "true":
+                up = True
+            case "false":
+                up = False
+            case _:
+                raise ValueError(f"invalid up value {elts[3]}")
+
+        return UptimeReport(
+            id=elts[0],
+            start_time_nanos=int(elts[1]),
+            end_time_nanos=int(elts[2]),
+            up=up,
+        )
+
+@dataclass
+class Charger:
+    id: str
+    uptime_reports: list[UptimeReport]
+
+@dataclass
+class Station:
+    id: str
+    charger_ids: list[str]
---
+++ test_data.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from data import UptimeReport
+from contextlib import AbstractContextManager, nullcontext as does_not_raise
+import pytest
+
+@dataclass
+class UptimeReportTestCase:
+    line: str
+    want_report: UptimeReport | None
+    expectation: AbstractContextManager
+
+uptime_report_test_cases: dict[str, UptimeReportTestCase] = {
+    "no_error": UptimeReportTestCase(
+        line="1001 0 50000 true",
+        want_report=UptimeReport(
+            id="1001",
+            start_time_nanos=0,
+            end_time_nanos=50000,
+            up=True,
+        ),
+        expectation=does_not_raise(),
+    ),
+    "three_elements_invalid": UptimeReportTestCase(
+        line="1001 0 50000",
+        want_report=None,
+        expectation=pytest.raises(ValueError),
+    ),
+    "five_elements_invalid": UptimeReportTestCase(
+        line="1001 0 50000 false extra",
+        want_report=None,
+        expectation=pytest.raises(ValueError),
+    ),
+}
+test_ids = sorted(uptime_report_test_cases.keys())
+@pytest.mark.parametrize(
+    "test_case",
+    [uptime_report_test_cases[test_id] for test_id in test_ids],
+    ids=test_ids,
+)
+def test_uptime_report_from_report_line(test_case: UptimeReportTestCase) -> None:
+    with test_case.expectation:
+        report = UptimeReport.from_report_line(test_case.line)
+        assert report == test_case.want_report
