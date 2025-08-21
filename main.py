ca1e6f09f3805700000ec6eb759a055c2c999cbe
alansu2024 <156165521+alansu2024@users.noreply.github.com>
2025-08-20 17:46:55 -0700

    bucket reports by charger

--- main.py
+++ main.py
@@ -24,19 +24,34 @@ def main() -> int:
             raise ValueError("expected [Stations] at the start of the file")
         
         lines = lines[1:]  # skip the header line
+        stations: list[Station] = []
         while lines[0] != "[Charger Availability Reports]":
             line, lines = lines[0], lines[1:]
             if line == "":
                 continue
             station = Station.from_station_line(line)
             print(f"Station {station.id} with chargers {station.charger_ids}")
+            stations.append(station)
+
 
         lines = lines[1:]  # skip the header line
+        reports: list[UptimeReport] = []
         for line in lines:
             if line == "":
                 continue
             report = UptimeReport.from_report_line(line)
             print(f"Report {report.id} from {report.start_time_nanos} to {report.end_time_nanos}, up: {report.up}")
+            reports.append(report)
+        reports_per_charger: dict[str, list[UptimeReport]] = {}
+        for report in reports:
+            if report.id not in reports_per_charger:
+                reports_per_charger[report.id] = []
+            reports_per_charger[report.id].append(report)
+        for charger, reports in reports_per_charger.items():
+            print(f"Charger {charger}")
+            for report in reports_per_charger[charger]:
+                print(f"  {report.start_time_nanos} to {report.end_time_nanos}, up: {report.up}")
+
 
     return 0
 
